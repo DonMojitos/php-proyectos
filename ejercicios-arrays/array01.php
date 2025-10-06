a39a3385a49cbc45d5286b88d82dec6f9547c220
Ivan <1801758l@alu.murciaeduca.es>
2025-10-06 13:35:36 +0200

    carpeta creDA DE ARRAYS

---
+++ ejercicios-arrays/array01.php
@@ -0,0 +1,7 @@
+<?php
+    $frase = "Es el vecino el que elige al alcalde y es el alcalde el que quiere que sean los vecinos el alcalde";
+    $arrFrase = explode(' ', $frase);
+    echo "<pre>" , print_r($arrFrase, true),"</pre>";
+
+    
+?>
--- ejerciciosTiempo/index.php
+++ ejerciciosTiempo/index.php
@@ -8,29 +8,35 @@
 </head>
 <body>
     <?php
+        $horaActual = date('h');
         $diaActual = date('d');
         $mesActual = date('m');
         $yearActual = date('Y');
+        $dias_totales = (int)10000/3;
+        $resto_horas = 10000%3;
         $tiempoActual = mktime(0, 0, 0, $mesActual, $diaActual, $yearActual);
+        $fechaFutura = mktime($horaActual + $resto_horas, 0, 0, $mesActual, $diaActual + $dias_totales, $yearActual);
         $fechaActual = date('d/m/Y', $tiempoActual);
-
-        $segundos = mktime(10000, 0, 0, $mesActual, $diaActual, $yearActual);
-        $fechaExperto = date('d/m/Y', $segundos);
+        $fechaExperto = date('d/m/Y', $fechaFutura);
 
         echo "<p>Serás un experto el $fechaExperto</p>";
 
         $timespan = mktime(0, 0, 0, 9, 17, 2013);
         $resta = $tiempoActual - $timespan;
         $diasTrans = $resta/86400;   
-        $mesTrans = $resta/2629743;   
-        $aniosTrans = $resta/31556926;   
+        $mesTrans = $diasTrans/30;   
+        $aniosTrans = $mesTrans/12;   
+        $aniosTrans = (int) ($mesTrans/12);
+        $mesTrans = (int) ($mesTrans%12);
+        $diasTrans = (int) ($diasTrans % 30);
         
         echo "<p>Desde la salida del GTA V han pasado: $diasTrans dias, $mesTrans meses y $aniosTrans años.</p>";
 
+
         $tiempoFuturo = mktime(0, 0, 0, $mesActual + 100, $diaActual + 100, $yearActual + 100);
         $fechaFuturo = date('Y-m-d', $tiempoFuturo);
-        $diaFuturo = date('D', $tiempoFuturo);
-        echo "<p>En 100 dias, 100 meses y 100 años será: $fechaFuturo y dia: $diaFuturo.</p>";
+        $diaFuturo = date('l', $tiempoFuturo);
+        echo "<p>En 100 dias, 100 meses y 100 años será: $fechaFuturo y dia $diaFuturo.</p>";
     ?>
 </body>
 </html>
